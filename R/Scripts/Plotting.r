5c7953a31461003028ffeea61a0dda6bbaa4165e
Renke Christian von Seggern <r.vonseggern@stud.uni-goettingen.de>
2017-04-27 18:23:00 +0200

    “Plotting.r”: added legend; adjusted .pdf dimensions to widescreen display (height = width*0.625); each “edvid” is plotted using a distinct combination of color and point character; plot includes only spruce (“art == 511”); added explanatory comments.

--- R/Scripts/Plotting.r
+++ R/Scripts/Plotting.r
@@ -13,8 +13,18 @@ bart$edvid <- as.factor(bart$edvid)
 ## 611 = Douglasie
 ## 711 = Kiefer
 
-## Plot basal area (gha) against Oberhöhe (h100), separately for each edvid
+#########################################################################################
+## Plot bart$gha against bart$h100 for bart$art == 511, separately for each bart$edvid ##
+#########################################################################################
 graphics.off()
+## Start graphics device driver for producing PDF graphics
+kPdfWidth <- 30
+pdf(file = "Graphics/gha_h100.pdf",
+    width = kPdfWidth,
+    height = kPdfWidth*0.625,
+    pointsize = 18,
+    family = "Times")
+## Create empty plot
 plot(x = NA,
      y = NA,
      xlab = "h100 [m]",
@@ -23,16 +33,39 @@ plot(x = NA,
      ylim = range(bart$gha, na.rm = TRUE),
      xaxs = "r",
      yaxs = "r")
-grid()
-n <- nlevels(bart$edvid)
-colnr <- 1
-colvec <- sample(x = colors(),
-                 size = n,
-                 replace = FALSE)
-for (ts in levels(bart$edvid)) {
-    points(x = bart$h100[bart$edvid == ts],
-           y = bart$gha[bart$edvid == ts],
-           col = colvec[colnr],
-           pch = 19)
-    colnr <- colnr+1
+grid(col = "black",
+     lwd = 2)
+## Create data frame containing combinations of col and pch
+kColVec <- c("black","green","red","brown","cyan","darkorange","darkblue","dimgray")
+kPchVec <- c(19,21:25)
+kPchColDataFrame <- data.frame()
+for (col in kColVec) {
+    kPchColDataFrame <- rbind(kPchColDataFrame,
+                              cbind(col = col,pch = kPchVec),
+                              stringsAsFactors = FALSE)
 }
+kPchColDataFrame$pch <- as.numeric(kPchColDataFrame$pch)
+kCntr <- 1
+## Add points to empty plot
+for (ts in levels(bart$edvid)) {
+    points(x = bart$h100[bart$edvid == ts & bart$art == 511],
+           y = bart$gha[bart$edvid == ts & bart$art == 511],
+           col = kPchColDataFrame$col[kCntr],
+           bg = kPchColDataFrame$col[kCntr],
+           pch = kPchColDataFrame$pch[kCntr])
+    kCntr <- kCntr+1
+}
+## Add legend
+legend(x = "topleft",
+       legend = paste("edvid: ", levels(bart$edvid)),
+       bg = "slategray1",
+       col = kPchColDataFrame$col,
+       pt.bg = kPchColDataFrame$col,
+       pch = kPchColDataFrame$pch,)
+graphics.off()
+## Open .pdf file via mupdf
+system2(command = "mupdf",
+        args = "-r 64 Graphics/gha_h100.pdf",
+        wait = FALSE)
+
+       
