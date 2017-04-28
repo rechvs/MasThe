f09b64a67511edcde35326d1241625e36ced69a6
Renke Christian von Seggern <r.vonseggern@stud.uni-goettingen.de>
2017-04-28 10:46:52 +0200

    “Plotting.r”: script now also plots “bart$ekl” against “bart$alt” for “bart$art == 511”.

--- R/Scripts/Plotting.r
+++ R/Scripts/Plotting.r
@@ -68,3 +68,59 @@ graphics.off()
 system2(command = "mupdf",
         args = "-r 64 Graphics/gha_h100.pdf",
         wait = FALSE)
+
+#########################################################################################
+## Plot bart$ekl against bart$alt for bart$art == 511, separately for each bart$edvid ##
+#########################################################################################
+graphics.off()
+## Start graphics device driver for producing PDF graphics
+kPdfWidth <- 30
+pdf(file = "Graphics/ekl_alt.pdf",
+    width = kPdfWidth,
+    height = kPdfWidth*0.625,
+    pointsize = 18,
+    family = "Times")
+## Create empty plot
+plot(x = NA,
+     y = NA,
+     xlab = "alt [a]",
+     ylab = "ekl",
+     xlim = c(range(bart$alt, na.rm = TRUE)[1],range(bart$alt, na.rm = TRUE)[2]+20),  ## accounts for extra space for placing the legend
+     ylim = range(bart$ekl, na.rm = TRUE),
+     xaxs = "r",
+     yaxs = "r",
+     main = "spruce only")
+grid(col = "black",
+     lwd = 2)
+## Create data frame containing combinations of col and pch
+kColVec <- c("black","green","red","brown","cyan","darkorange","burlywood","dimgray","yellow","magenta")
+kPchVec <- c(21:25)
+kPchColDataFrame <- data.frame()
+for (col in kColVec) {
+    kPchColDataFrame <- rbind(kPchColDataFrame,
+                              cbind(col = col,pch = kPchVec),
+                              stringsAsFactors = FALSE)
+}
+kPchColDataFrame$pch <- as.numeric(kPchColDataFrame$pch)
+kCntr <- 1
+## Add points to empty plot
+for (ts in levels(bart$edvid)) {
+    points(x = bart$alt[bart$edvid == ts & bart$art == 511],
+           y = bart$ekl[bart$edvid == ts & bart$art == 511],
+           col = kPchColDataFrame$col[kCntr],
+           bg = kPchColDataFrame$col[kCntr],
+           pch = kPchColDataFrame$pch[kCntr])
+    kCntr <- kCntr+1
+}
+## Add legend
+legend(x = "topright",
+       legend = paste("edvid: ", levels(bart$edvid)),
+       bg = "slategray1",
+       col = kPchColDataFrame$col,
+       pt.bg = kPchColDataFrame$col,
+       pch = kPchColDataFrame$pch,)
+graphics.off()
+## Open .pdf file via mupdf
+system2(command = "mupdf",
+        args = "-r 64 Graphics/ekl_alt.pdf",
+        wait = FALSE)
