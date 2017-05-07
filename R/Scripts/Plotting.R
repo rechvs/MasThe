3e6c56113ef6df82e1d103e8fa0a34a679e633ea
Renke Christian von Seggern <r.vonseggern@stud.uni-goettingen.de>
2017-05-07 12:36:20 +0200

    “Plotting.R”: added periods at the end of comments; changed loading of base file; removed obsolete change of columns of “bart” to factor.

--- R/Scripts/Plotting.R
+++ R/Scripts/Plotting.R
@@ -1,30 +1,33 @@
-## Preamble
+###############
+## Preamble. ##
+###############
 rm(list = ls())
 setwd(dir = "~/laptop02_MasAr")
+## Load base file.
+kBaseFileVersion <- "1.5"
+kBaseFileName <- paste0(kDataDir,"gmax_", kBaseFileVersion, ".RData")
+kgmaxObjects <- load(file = kBaseFileName, verbose = TRUE)
 load(file = "R/Data/gmax.RData")
 
-## Transform columns to factor if appropriate
-bart$edvid <- as.factor(bart$edvid)
-
-## Tree species according to Wördehoff (2016)
+## Tree species according to Wördehoff (2016).
 ## 110 = Eiche
 ## 211 = Buche
 ## 511 = Fichte
 ## 611 = Douglasie
 ## 711 = Kiefer
 
-#########################################################################################
-## Plot bart$gha against bart$h100 for bart$art == 511, separately for each bart$edvid ##
-#########################################################################################
+##########################################################################################
+## Plot bart$gha against bart$h100 for bart$art == 511, separately for each bart$edvid. ##
+##########################################################################################
 graphics.off()
-## Start graphics device driver for producing PDF graphics
+## Start graphics device driver for producing PDF graphics.
 kPdfWidth <- 30
 pdf(file = "Graphics/gha_h100.pdf",
     width = kPdfWidth,
     height = kPdfWidth*0.625,
     pointsize = 18,
     family = "Times")
-## Create empty plot
+## Create empty plot.
 plot(x = NA,
      y = NA,
      xlab = "h100 [m]",
@@ -36,7 +39,7 @@ plot(x = NA,
      main = "spruce only")
 grid(col = "black",
      lwd = 2)
-## Create data frame containing combinations of col and pch
+## Create data frame containing combinations of col and pch.
 kColVec <- c("black","green","red","brown","cyan","darkorange","burlywood","dimgray","yellow","magenta")
 kPchVec <- c(21:25)
 kPchColDataFrame <- data.frame()
@@ -47,7 +50,7 @@ for (col in kColVec) {
 }
 kPchColDataFrame$pch <- as.numeric(kPchColDataFrame$pch)
 kCntr <- 1
-## Add points to empty plot
+## Add points to empty plot.
 for (ts in levels(bart$edvid)) {
     points(x = bart$h100[bart$edvid == ts & bart$art == 511],
            y = bart$gha[bart$edvid == ts & bart$art == 511],
@@ -56,7 +59,7 @@ for (ts in levels(bart$edvid)) {
            pch = kPchColDataFrame$pch[kCntr])
     kCntr <- kCntr+1
 }
-## Add legend
+## Add legend.
 legend(x = "topleft",
        legend = paste("edvid: ", levels(bart$edvid)),
        bg = "slategray1",
@@ -64,35 +67,35 @@ legend(x = "topleft",
        pt.bg = kPchColDataFrame$col,
        pch = kPchColDataFrame$pch,)
 graphics.off()
-## Open .pdf file via mupdf
+## Open .pdf file via mupdf.
 system2(command = "mupdf",
         args = "-r 64 Graphics/gha_h100.pdf",
         wait = FALSE)
 
 #########################################################################################
-## Plot bart$ekl against bart$alt for bart$art == 511, separately for each bart$edvid ##
+## Plot bart$ekl against bart$alt for bart$art == 511, separately for each bart$edvid. ##
 #########################################################################################
 graphics.off()
-## Start graphics device driver for producing PDF graphics
+## Start graphics device driver for producing PDF graphics.
 kPdfWidth <- 30
 pdf(file = "Graphics/ekl_alt.pdf",
     width = kPdfWidth,
     height = kPdfWidth*0.625,
     pointsize = 18,
     family = "Times")
-## Create empty plot
+## Create empty plot.
 plot(x = NA,
      y = NA,
      xlab = "alt [a]",
      ylab = "ekl",
-     xlim = c(range(bart$alt, na.rm = TRUE)[1],range(bart$alt, na.rm = TRUE)[2]+20),  ## accounts for extra space for placing the legend
+     xlim = c(range(bart$alt, na.rm = TRUE)[1],range(bart$alt, na.rm = TRUE)[2]+20),  ## accounts for extra space for placing the legend.
      ylim = range(bart$ekl, na.rm = TRUE),
      xaxs = "r",
      yaxs = "r",
      main = "spruce only")
 grid(col = "black",
      lwd = 2)
-## Create data frame containing combinations of col and pch
+## Create data frame containing combinations of col and pch.
 kColVec <- c("black","green","red","brown","cyan","darkorange","burlywood","dimgray","yellow","magenta")
 kPchVec <- c(21:25)
 kPchColDataFrame <- data.frame()
@@ -103,7 +106,7 @@ for (col in kColVec) {
 }
 kPchColDataFrame$pch <- as.numeric(kPchColDataFrame$pch)
 kCntr <- 1
-## Add points to empty plot
+## Add points to empty plot.
 for (ts in levels(bart$edvid)) {
     points(x = bart$alt[bart$edvid == ts & bart$art == 511],
            y = bart$ekl[bart$edvid == ts & bart$art == 511],
@@ -112,7 +115,7 @@ for (ts in levels(bart$edvid)) {
            pch = kPchColDataFrame$pch[kCntr])
     kCntr <- kCntr+1
 }
-## Add legend
+## Add legend.
 legend(x = "topright",
        legend = paste("edvid: ", levels(bart$edvid)),
        bg = "slategray1",
@@ -120,7 +123,7 @@ legend(x = "topright",
        pt.bg = kPchColDataFrame$col,
        pch = kPchColDataFrame$pch,)
 graphics.off()
-## Open .pdf file via mupdf
+## Open .pdf file via mupdf.
 system2(command = "mupdf",
         args = "-r 64 Graphics/ekl_alt.pdf",
         wait = FALSE)
