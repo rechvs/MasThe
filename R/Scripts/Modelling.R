b368094afeca8ab295eefbc6dbc988a1dd53e086
Renke Christian von Seggern <r.vonseggern@stud.uni-goettingen.de>
2017-05-18 13:11:53 +0200

    “Modelling.R”: started tracking.

---
+++ R/Scripts/Modelling.R
@@ -0,0 +1,33 @@
+##############
+## Preamble ##
+##############
+rm(list = ls())
+setwd(dir = "~/laptop02_MasAr")
+kDataDir <- "Data/"
+{sink(file = "/dev/null"); source(file = "R/Scripts/DataSetCreation.R"); sink()}  ## Create up-to-date data sets  while suppressing output.
+## Load base file.
+kFileVersion <- "2.2"
+kFileName <- paste0(kDataDir, "gmax_", kFileVersion, ".RData")
+kgmaxObjects <- load(file = kFileName, verbose = TRUE)
+
+###############################################
+## Model "gha" using "SI.h100" as predictor. ##
+###############################################
+## Load required libraries.
+library("mgcv")
+
+## Evaluate models.
+gam_gha_h100 <- gam(data = bart.clean,
+                    formula = gha ~ s(h100))
+gam_gha_h100.EKL.I<- gam(data = bart.clean,
+                         formula = gha ~ s(h100.EKL.I))
+gam_gha_SI.h100 <- gam(data = bart.clean,
+                       formula = gha ~ s(SI.h100))
+
+## BEGIN TESTING ##
+graphics.off()
+plot.gam(x = gam_gha_h100,
+         pages = 1,
+         seWithMean = TRUE,
+         all.terms = TRUE)
+## END TESTING ##
