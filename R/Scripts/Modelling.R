7f193335f437fba667906b0514bd911f5c544e71
Renke Christian von Seggern <r.vonseggern@stud.uni-goettingen.de>
2017-05-19 10:35:59 +0200

    “Modelling.R”: adjusted the code to ease calling the script from outside.

--- R/Scripts/Modelling.R
+++ R/Scripts/Modelling.R
@@ -5,30 +5,23 @@ rm(list = ls())
 setwd(dir = "~/laptop02_MasAr")
 kDataDir <- "Data/"
 {sink(file = "/dev/null"); source(file = "R/Scripts/DataSetCreation.R"); sink()}  ## Create up-to-date data sets  while suppressing output.
-## Load base file.
+## Load data set.
 kFileVersion <- "2.2"
 kFileName <- paste0(kDataDir, "gmax_", kFileVersion, ".RData")
 kgmaxObjects <- load(file = kFileName, verbose = TRUE)
 
-###############################################
-## Model "gha" using "SI.h100" as predictor. ##
-###############################################
-## Load required libraries.
-library("mgcv")
-
-## Evaluate models.
-gam_gha_h100 <- gam(data = bart.clean,
-                    formula = gha ~ s(h100, k = 5))
-gam_gha_h100.EKL.I<- gam(data = bart.clean,
-                         formula = gha ~ s(h100.EKL.I, k = 5))
-gam_gha_SI.h100 <- gam(data = bart.clean,
-                       formula = gha ~ s(SI.h100, k = 26))
-
-## BEGIN TESTING ##
-graphics.off()
-dev.new()
-plot.gam(x = gam_gha_h100,
-         pages = 1,
-         seWithMean = TRUE,
-         all.terms = TRUE)
-## END TESTING ##
+######################################################################
+## Evaluate and store models in object "models"                     ##
+## (in order to be able to retrieve them from outside this script). ##
+######################################################################
+## Modelling preamble ##
+library("mgcv")  ## Load required libraries.
+## n <- 3  ## Number of models to be stored.
+## models <- vector(mode = "list", length = n)  ## Create empty list of correct length in which to store the models.
+## Evaluate and store models.
+models <- list("gam_gha_sh100" = gam(data = bart.clean,
+                                   formula = gha ~ s(h100, k = 5)),
+               "gam_gha_sh100.EKL.I" = gam(data = bart.clean,
+                                         formula = gha ~ s(h100.EKL.I, k = 5)),
+               "gam_gha_sSI.h100" = gam(data = bart.clean,
+                                        formula = gha ~ s(SI.h100, k = 26)))
