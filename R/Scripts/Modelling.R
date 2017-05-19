45e0277a05b4e0bff6dc23801a6a5e0f0b68978f
Renke Christian von Seggern <r.vonseggern@stud.uni-goettingen.de>
2017-05-19 16:48:31 +0200

    “Modelling.R” block “GAMs”: rewrote block according to block “Sterba”. “Modelling.R” block “Sterba”: uncommented erroneously commented line; moved printing of model summaries into separate block “Print summaries”.

--- R/Scripts/Modelling.R
+++ R/Scripts/Modelling.R
@@ -12,21 +12,25 @@ kgmaxObjects <- load(file = kFileName, verbose = TRUE)
 models <- vector(mode = "list")
 formulas <- vector(mode = "list")
 start.vals <- vector(mode = "list")
+print.sumries <- TRUE
+print.sumries <- FALSE
 
 ##########
 ## GAMs ##
 ##########
-## Modelling preamble ##
-library("mgcv")  ## Load required libraries.
-## n <- 3  ## Number of models to be stored.
-## models <- vector(mode = "list", length = n)  ## Create empty list of correct length in which to store the models.
+## Modelling preamble.
+library("mgcv")
+## Setup for model "GAM_gha_sh100".
+formulas[["GAM_gha_sh100"]] <- as.formula(object = "gha ~ s(h100, k = 5)")
+## Setup for model "GAM_gha_sh100.EKL.I".
+formulas[["GAM_gha_sh100.EKL.I"]] <- as.formula(object = "gha ~ s(h100.EKL.I, k = 5)")
+## Setup for model ""GAM_gha_sSI.h100"".
+formulas[["GAM_gha_sSI.h100"]] <- as.formula(object = "gha ~ s(SI.h100, k = 26)")
 ## Evaluate and store models.
-models <- list("gam_gha_sh100" = gam(data = bart.clean,
-                                   formula = gha ~ s(h100, k = 5)),
-               "gam_gha_sh100.EKL.I" = gam(data = bart.clean,
-                                         formula = gha ~ s(h100.EKL.I, k = 5)),
-               "gam_gha_sSI.h100" = gam(data = bart.clean,
-                                        formula = gha ~ s(SI.h100, k = 26)))
+for (cur.formula.name in names(x = formulas)) {
+    models[[cur.formula.name]] <- gam(formula = formulas[[cur.formula.name]],
+                                      data = bart.clean)
+}
 
 ############
 ## Sterba ##
@@ -34,11 +38,11 @@ models <- list("gam_gha_sh100" = gam(data = bart.clean,
 ## Modelling preamble.
 library(package = "nls2")
 ## Setup for model "Sterba_dgGmax".
-## formulas[["Sterba_dgGmax"]] <- as.formula(object = "dg ~ 1 / (a0 * (h100 ^ a1) * nha + b0 * (h100 ^ b1))")
-## start.vals[["Sterba_dgGmax"]] <- expand.grid("a0" = c(-2, 10),
-                                             ## "a1" = c(-2, 10),
-                                             ## "b0" = c(-2, 10),
-                                             ## "b1" = c(-2, 10))
+formulas[["Sterba_dgGmax"]] <- as.formula(object = "dg ~ 1 / (a0 * (h100 ^ a1) * nha + b0 * (h100 ^ b1))")
+start.vals[["Sterba_dgGmax"]] <- expand.grid("a0" = c(-2, 10),
+                                             "a1" = c(-2, 10),
+                                             "b0" = c(-2, 10),
+                                             "b1" = c(-2, 10))
 ## Setup for model "Sterba_Gmax".
 formulas[["Sterba_Gmax"]] <- as.formula(object = "gha ~ pi / (16 * a0 * b0 * (h100 ^(a1 + b1)))")
 start.vals[["Sterba_Gmax"]] <- expand.grid("a0" = c(4 * 10 -6, 1),  ## cp. Wördehoff (2016), tab. 3.6
@@ -51,11 +55,14 @@ for (cur.formula.name in names(x = formulas)) {
                                        data = bart.clean,
                                        start = start.vals[[cur.formula.name]])
 }
+
+#####################
+## Print summaries ##
+#####################
 ## Print model summaries.
-print.cor <- FALSE
-## print.cor <- TRUE
-for (cur.model.name in names(x = models)) {
-    print(x = paste0("Model: ", cur.model.name))
-    print(x = summary(object = models[[cur.model.name]],
-                      correlation = print.cor))
+if (print.sumries) {
+    for (cur.model.name in names(x = models)) {
+        print(x = paste0("Model: ", cur.model.name))
+        print(x = summary(object = models[[cur.model.name]]))
+    }
 }
