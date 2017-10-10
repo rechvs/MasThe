f6f9ae50ca288bd8b974e9f8c26ff51d374409d3
Renke Christian von Seggern <r.vonseggern@stud.uni-goettingen.de>
2017-10-10 13:00:18 +0200

    “MasAr_Thesis_Figures.R” block “log(N)-log(D)-plot before/after data selection”: renamed block to “log(N)-log(D)-plots before/after data selection”; did some fine-tuning.

--- R/Scripts/MasAr_Thesis_Figures.R
+++ R/Scripts/MasAr_Thesis_Figures.R
@@ -9,23 +9,26 @@ kDataDir <- "Data/"
 kBaseFileVersion <- "5.3"
 kBaseFileName <- paste0(kDataDir, "gmax_merged_", kBaseFileVersion, ".RData")
 kgmaxObjects <- load(file = kBaseFileName, verbose = TRUE)
+## If nonexistent, create subdirectory in which to store graphics.
+kGraphicsSubdir <- "Graphics/Thesis/"
+system2(command = "mkdir",
+        args = paste0("-p ", kGraphicsSubdir))
 objects.at.script.start <- c(ls(), "objects.at.script.start")  ## Required for cleaning up workspace.
 
-####################################################
-## log(N)-log(D)-plot before/after data selection ##
-####################################################
+#####################################################
+## log(N)-log(D)-plots before/after data selection ##
+#####################################################
 ## TEX file: ~/laptop02_MasAr/LaTeX/Thesis/Data_Selection.tex
+## This block creates 2 files, one per species. Each file contains 2 plots (in a 1 row, 2 column layout): the left plot for observations before application of the data selection mechanism, the right plot for observations after application of the data selection mechanism.
 ## Plotting preamble.
 kPdfWidth <- (210 - 30 - 20) * 0.3937
 kPdfHeight <- kPdfWidth * 0.55
-kPdfPointSize <- 50
+kPdfPointSize <- 60
 kPdfFamily <- "Times"
 kPlotMargins <- c(4.1, 4.2, 1.5, 0.1)  ## As small as possible using fractions of lines.
-## kPlotMargins <- c(5, 5, 2, 1)  ## As small as possible using whole lines.
 kLineWidth <- 4
 kLineType <- "solid"
-kPointCharacters <- list("beech" = 19,
-                         "spruce" = 19)
+kPointCharacter <- 19
 kXLab <- "log(D)"
 kYLab <- "log(N)"
 kXMin <- 0.5
@@ -47,49 +50,34 @@ kSpeciesNames <- c("beech", "spruce")
 kPointLineColors <- list("beech" = "#ee7f00",
                          "spruce" = "#4066aa")
 kPlotLayout <- 1:2
-
-## If nonexistent, create subdirectory in which to store graphics.
-graphics.subdir <- "Graphics/Thesis/"
-system2(command = "mkdir",
-        args = paste0("-p ", graphics.subdir))
-
 ## Loop over all species names.
 for (cur.species.name in kSpeciesNames) {
-
     ## Turn off graphics device.
     graphics.off()
-
     ## Create file name.
-    file.name <-paste0(graphics.subdir,
+    file.name <-paste0(kGraphicsSubdir,
                        cur.species.name,
                        "_logN_logD_Plot_before_after_data_selection.pdf")
-
     ## Start graphics device driver for producing PDF graphics.
     pdf(file = file.name,
         width = kPdfWidth,
         height = kPdfHeight,
         pointsize = kPdfPointSize,
         family = kPdfFamily)
-
+    ## Set plot margins.
+    par("mar" = kPlotMargins)
     ## Set plot layout to 1 row and 2 columns (1 graphics device per species, 1 plot per data selection state).
     layout(mat = matrix(data = kPlotLayout,
                         nrow = 1,
                         ncol = 2,
                         byrow = FALSE))
-
     ## Loop over all plot numbers.
     for (cur.plot.nr in kPlotLayout) {
-        ## Set species name for current plot number ("beech" for plot numbers 1 and 2, "spruce" for plot numbers 3 and 4).
-        ## cur.species.name <- ifelse(test = cur.plot.nr <= 2,
-        ## yes = "beech",
-        ## no = "spruce")
         ## Extract x- and y-limits for current species.
         cur.xmin <- as.numeric(x = kXMin[names(x = kXMin) == cur.species.name])
         cur.xmax <- as.numeric(x = kXMax[names(x = kXMax) == cur.species.name])
         cur.ymin <- as.numeric(x = kYMin[names(x = kYMin) == cur.species.name])
         cur.ymax <- as.numeric(x = kYMax[names(x = kYMax) == cur.species.name])
-        ## Set point character for current species.
-        cur.point.character <- as.numeric(x = kPointCharacters[names(x = kPointCharacters) == cur.species.name])
         ## Set point and line color for current species.
         cur.point.line.col <- as.character(x = kPointLineColors[names(x = kPointLineColors) == cur.species.name])
         ## Set data frame version for current plot number ("1.0" for plot number 1, "1.8" for plot number 2).
@@ -119,7 +107,6 @@ for (cur.species.name in kSpeciesNames) {
                                   lty = kGridLineType,
                                   lwd = kGridLineWidth,
                                   col = kGridLineCol))
-        
         ## Loop over all "edvid"s.
         for (cur.edvid.name in levels(x = cur.data.frame[["edvid"]])) {
             ## Extract x-values for current "edvid".
@@ -130,7 +117,7 @@ for (cur.species.name in kSpeciesNames) {
             points(x = cur.x.values,
                    y = cur.y.values,
                    type = "p",
-                   pch = cur.point.character,
+                   pch = kPointCharacter,
                    lty = kLineType,
                    lwd = kLineWidth,
                    col = cur.point.line.col)
@@ -140,10 +127,17 @@ for (cur.species.name in kSpeciesNames) {
                   lty = kLineType,
                   lwd = kLineWidth,
                   col = cur.point.line.col)
+            ## Add legend.
+            ## legend(x = "topright",
+            ## legend = cur.species.name,
+            ## col = cur.point.line.col,
+            ## lty = kLineType,
+            ## pch = kPointCharacter,
+            ## lwd = kLineWidth,
+            ## bg = "gray")
         }}
-    
     ## Turn off graphics device.
     graphics.off()
-
 }
-
+## Clean up workspace.
+rm(list = setdiff(x = ls(), y = objects.at.script.start))
