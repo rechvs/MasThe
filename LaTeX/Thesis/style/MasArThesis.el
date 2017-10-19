94ffb41bed60a2bcc95697615af3632dda66c566
Renke Christian von Seggern <r.vonseggern@stud.uni-goettingen.de>
2017-10-19 14:04:17 +0200

    “MasArThesis.el”: updated.

--- LaTeX/Thesis/style/MasArThesis.el
+++ LaTeX/Thesis/style/MasArThesis.el
@@ -7,7 +7,7 @@
  LaTeX-dialect)
 
 ;; Add macro "\RefEq" as a macro requiring a single mandatory argument to macro class "font-latex-match-reference-keywords" for syntax highlighting. See AUCTeX info Fontification of macros.
-(add-to-list 'font-latex-match-reference-keywords '("refEq" "{"))
+(add-to-list 'font-latex-match-reference-keywords '("RefEq" "{"))
 
 ;; Make certain macros known to AUCTeX as macros to which to append an empty set of braces.
 (TeX-add-style-hook
@@ -27,22 +27,22 @@
  "MasArThesis"
  (lambda ()
    (TeX-add-symbols
-    '("refTab" TeX-arg-ref)))
+    '("RefTab" TeX-arg-ref)))
  LaTeX-dialect)
 
 ;; Add macro "\RefTab" as a macro requiring a single mandatory argument to macro class "font-latex-match-reference-keywords" for syntax highlighting. See AUCTeX info Fontification of macros.
-(add-to-list 'font-latex-match-reference-keywords '("refTab" "{"))
+(add-to-list 'font-latex-match-reference-keywords '("RefTab" "{"))
 
 ;; Make macro "\RefFig" known to AUCTeX as a macro requiring a label as the argument. See AUCTeX info Adding Macros. See variable "TeX-symbol-list" (which is buffer local, so you have to issue "C-h v" in the buffer visiting a tex file related to the master file in question) if it worked as intended.
 (TeX-add-style-hook
  "MasArThesis"
  (lambda ()
    (TeX-add-symbols
-    '("refFig" TeX-arg-ref)))
+    '("RefFig" TeX-arg-ref)))
  LaTeX-dialect)
 
 ;; Add macro "\RefFig" as a macro requiring a single mandatory argument to macro class "font-latex-match-reference-keywords" for syntax highlighting. See AUCTeX info Fontification of macros.
-(add-to-list 'font-latex-match-reference-keywords '("refFig" "{"))
+(add-to-list 'font-latex-match-reference-keywords '("RefFig" "{"))
 
 ;; Make environment "longtabu" known to AUCTeX as a table environemnt requiring:
 ;; - 
