ad8ea9df82f7af566eed1898693a9da1cd18d2be
Renke Christian von Seggern <r.vonseggern@stud.uni-goettingen.de>
2017-11-13 13:17:58 +0100

    “MasArThesis.el”: updated; rearranged code so that all macros requiring a label as the argument are now added in a single call via “TeX-add-style-hook” and “TeX-add-symbols” (instead of one call per macro); moved coded which adds macros to list “font-latex-match-reference-keywords” into a single paragraph (instead of one paragraph per macro).

--- LaTeX/Thesis/style/MasArThesis.el
+++ LaTeX/Thesis/style/MasArThesis.el
@@ -1,13 +1,21 @@
-;; Make macro "\RefEq" known to AUCTeX as a macro requiring a label as the argument. See AUCTeX info Adding Macros. See variable "TeX-symbol-list" (which is buffer local, so you have to issue "C-h v" in the buffer visiting a tex file related to the master file in question) if it worked as intended.
+;; Make certain macros known to AUCTeX as macros requiring a label as the argument. See AUCTeX info Adding Macros. See variable "TeX-symbol-list" (which is buffer local, so you have to issue "C-h v" in the buffer visiting a tex file related to the master file in question) if it worked as intended.
 (TeX-add-style-hook
  "MasArThesis"
  (lambda ()
    (TeX-add-symbols
-    '("RefEq" TeX-arg-ref)))
+    '("RefEq" TeX-arg-ref)
+    '("RefFig" TeX-arg-ref)
+    '("RefTab" TeX-arg-ref)
+    '("SeePage" TeX-arg-ref)
+    '("SeeSection" TeX-arg-ref)))
  LaTeX-dialect)
 
-;; Add macro "\RefEq" as a macro requiring a single mandatory argument to macro class "font-latex-match-reference-keywords" for syntax highlighting. See AUCTeX info Fontification of macros.
+;; Add certain macros as macros requiring a single mandatory argument to macro class "font-latex-match-reference-keywords" for syntax highlighting. See AUCTeX info Fontification of macros.
 (add-to-list 'font-latex-match-reference-keywords '("RefEq" "{"))
+(add-to-list 'font-latex-match-reference-keywords '("RefFig" "{"))
+(add-to-list 'font-latex-match-reference-keywords '("RefTab" "{"))
+(add-to-list 'font-latex-match-reference-keywords '("SeePage" "{"))
+(add-to-list 'font-latex-match-reference-keywords '("SeeSection" "{"))
 
 ;; Make certain macros known to AUCTeX as macros to which to append an empty set of braces.
 (TeX-add-style-hook
@@ -38,28 +46,6 @@
    (TeX-add-symbols
     '("interval" 2)))  ;; See package "interval".
  LaTeX-dialect)
-
-;; Make macro "\RefTab" known to AUCTeX as a macro requiring a label as the argument. See AUCTeX info Adding Macros. See variable "TeX-symbol-list" (which is buffer local, so you have to issue "C-h v" in the buffer visiting a tex file related to the master file in question) if it worked as intended.
-(TeX-add-style-hook
- "MasArThesis"
- (lambda ()
-   (TeX-add-symbols
-    '("RefTab" TeX-arg-ref)))
- LaTeX-dialect)
-
-;; Add macro "\RefTab" as a macro requiring a single mandatory argument to macro class "font-latex-match-reference-keywords" for syntax highlighting. See AUCTeX info Fontification of macros.
-(add-to-list 'font-latex-match-reference-keywords '("RefTab" "{"))
-
-;; Make macro "\RefFig" known to AUCTeX as a macro requiring a label as the argument. See AUCTeX info Adding Macros. See variable "TeX-symbol-list" (which is buffer local, so you have to issue "C-h v" in the buffer visiting a tex file related to the master file in question) if it worked as intended.
-(TeX-add-style-hook
- "MasArThesis"
- (lambda ()
-   (TeX-add-symbols
-    '("RefFig" TeX-arg-ref)))
- LaTeX-dialect)
-
-;; Add macro "\RefFig" as a macro requiring a single mandatory argument to macro class "font-latex-match-reference-keywords" for syntax highlighting. See AUCTeX info Fontification of macros.
-(add-to-list 'font-latex-match-reference-keywords '("RefFig" "{"))
 
 ;; Make environment "longtabu" known to AUCTeX as a table environemnt requiring:
 ;; - 
